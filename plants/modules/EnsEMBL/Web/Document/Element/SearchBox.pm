c2f886642a44d4d23d07b75fc8387f1ff6795877
Nick Langridge <nicklangridge@hotmail.com>
2015-12-17 11:31:16 +0000

    Only show search box on species pages

--- plants/modules/EnsEMBL/Web/Document/Element/SearchBox.pm
+++ plants/modules/EnsEMBL/Web/Document/Element/SearchBox.pm
@@ -18,21 +18,26 @@ limitations under the License.
 
 package EnsEMBL::Web::Document::Element::SearchBox;
 
-### Generates small search box (used in top left corner of pages)
-
 use strict;
+use previous qw(content);
 
+# limit search options
 sub search_options {
   my $sitename = $_[0]->species_defs->SITE_NAME;
 
   return [
     ($_[0]->hub->species and $_[0]->hub->species !~ /^(common|multi)$/i) ? (
     'ensemblthis'     => { 'label' => 'Search ' . $_[0]->species_defs->SPECIES_COMMON_NAME, 'icon' => 'search/ensembl.gif'  }) : (),
-    'ensemblunit'     => { 'label' => "Search all species",     'icon' => 'search/ensemblunit.gif'      },
     'ensembl_genomes' => { 'label' => 'Search Ensembl Genomes', 'icon' => 'search/ensembl_genomes.gif'  },
-    'ensembl_all'     => { 'label' => 'Search all species',     'icon' => 'search/ensembl.gif'          },
     'ebi'             => { 'label' => 'Search EBI',             'icon' => 'search/ebi.gif'              },
   ];
+}
+
+# only show search box on a species page
+sub content {
+  my $self = shift;
+  my $species = $self->species;
+  return $species ? $self->PREV::content(@_) : '';
 }
 
 1;
